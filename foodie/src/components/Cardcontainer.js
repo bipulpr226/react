a36ab7acfa142681c6a3b22e5cacea7e9c805a62
unknown <pradhanbipul125@gmail.com>
2024-06-12 20:49:28 +0530

    second commit

--- foodie/src/components/Cardcontainer.js
+++ foodie/src/components/Cardcontainer.js
@@ -1,5 +1,5 @@
  import Restaurantcard from "./Restaurantcard";
- //import { restaurantList } from "../const/config";
+
  import {useEffect, useState} from 'react'
 import Shimmer from "./Shimmer";
 
@@ -54,12 +54,7 @@ if (loading){
  onChange={handleSearchText} />
 <button className="btn btn-sm btn-light" onClick={filterData}>🔍</button>
         </div>
-        {/* <input type="text" onChange={(e)=>{text=e.target.value;
-          console.log("text",text)
-        }}/> */}
-   {/* <button  class="button" onClick={filterRestaurants}>Top Rated Restaurants</button>
-   <h1>Count is {count}</h1>
-   <button class="button" onClick={()=>setcount(count+1)}>Increment</button> */}
+       
 <div className="container d-flex flex-wrap gap-5">
 
 
@@ -68,12 +63,7 @@ return(
 
   <Restaurantcard 
   key={restaurant?.info?.id}
-  //  imgUrl={IMG_URL+restaurant?.info?.cloudinaryImageId}
-  // title={restaurant?.info?.name}
-  // starRating={restaurant?.info?.starRating}
-  // deliveryTime={restaurant?.info?.sla?.deliveryTime}
-  // cuisines={restaurant?.info?.cuisines.join(",")}
-  // location={restaurant?.info?.areaName}
+ 
   {...restaurant?.info}
   />
 );
